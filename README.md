b68a4d29d60523ec377d3d3e7519f4b9bf5cd25b
杨烨 <imissyou5201314@outlook.com>
2021-11-28 16:15:29 +0800

    添加数据库文件

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# jqby
-新华机器博弈论坛源码
+# 新华机器博弈论坛
+
+### 介绍
+项目架子主要依赖：
+- SpringBoot 2.3.0
+- MySQL 5.7
+- MyBatisPlus 3.0.5
+- swagger
+- fastjson
+- thymeleaf
+- editormd
+- layer
+- bootstrap
+- springsecurity
+- 代码自动生成
+
+### 安装教程
+
+1.  执行SQL，创建数据库
+2.  项目导入IDEA 启动
+3.  访问测试！
+4.  自行根据代码逻辑填充测试数据，查看即可！
+
+### 目录说明
+```shell script
+src/main/java/com/kuang
+- config        配置类
+- controller    controller
+- generator     代码生成器
+- mapper        mapper
+- pojo          pojo
+- result        统一返回结果
+- service       service
+- utils         工具类
+- vo            vo对象
+KuangstudyApplication   启动类
+
+src/resources
+- static        静态资源
+- templates     页面模板
+- application-*.properties   配置文件
+
+jqby.sql   数据库文件
+```
+
---
+++ jqby.sql
@@ -0,0 +1,317 @@
+/*
+SQLyog Enterprise v12.09 (64 bit)
+MySQL - 5.7.35-log : Database - kuangstudy
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`kuangstudy` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
+
+USE `kuangstudy`;
+
+/*Table structure for table `ks_blog` */
+
+DROP TABLE IF EXISTS `ks_blog`;
+
+CREATE TABLE `ks_blog` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `bid` varchar(200) NOT NULL COMMENT '博客id',
+  `title` varchar(200) NOT NULL COMMENT '博客标题',
+  `content` longtext NOT NULL COMMENT '博客内容',
+  `sort` int(1) NOT NULL DEFAULT '0' COMMENT '排序 0 普通  1 置顶',
+  `views` int(10) NOT NULL DEFAULT '0' COMMENT '浏览量',
+  `author_id` varchar(200) NOT NULL COMMENT '作者id',
+  `author_name` varchar(200) NOT NULL DEFAULT '这小子没填资料' COMMENT '作者名',
+  `author_avatar` varchar(500) NOT NULL COMMENT '作者头像',
+  `category_id` int(10) NOT NULL COMMENT '问题分类id',
+  `category_name` varchar(50) NOT NULL COMMENT '问题分类名称',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_update` datetime NOT NULL COMMENT '修改时间',
+  `author_real_name` varchar(200) DEFAULT '这小子没填资料',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_blog_category` */
+
+DROP TABLE IF EXISTS `ks_blog_category`;
+
+CREATE TABLE `ks_blog_category` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `category` varchar(50) NOT NULL COMMENT '博客分类',
+  `gmt_create` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_comment` */
+
+DROP TABLE IF EXISTS `ks_comment`;
+
+CREATE TABLE `ks_comment` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `comment_id` varchar(200) NOT NULL COMMENT '评论唯一id',
+  `topic_category` int(1) NOT NULL COMMENT '1博客 2问答',
+  `topic_id` varchar(200) NOT NULL COMMENT '评论主题id',
+  `user_id` varchar(200) NOT NULL COMMENT '评论者id',
+  `user_name` varchar(200) NOT NULL COMMENT '评论者昵称',
+  `user_avatar` varchar(500) NOT NULL COMMENT '评论者头像',
+  `content` longtext NOT NULL COMMENT '评论内容',
+  `gmt_create` datetime NOT NULL COMMENT '评论创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_download` */
+
+DROP TABLE IF EXISTS `ks_download`;
+
+CREATE TABLE `ks_download` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(100) NOT NULL COMMENT '资源名',
+  `ddesc` varchar(500) NOT NULL COMMENT '资源链接',
+  `dcode` varchar(50) NOT NULL COMMENT '提取码',
+  `gmt_create` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_invite` */
+
+DROP TABLE IF EXISTS `ks_invite`;
+
+CREATE TABLE `ks_invite` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `code` varchar(200) NOT NULL COMMENT '邀请码',
+  `uid` varchar(200) DEFAULT NULL COMMENT '用户id',
+  `status` int(1) NOT NULL DEFAULT '0' COMMENT '状态 0 未使用 1 使用',
+  `active_time` datetime DEFAULT NULL COMMENT '激活时间',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_question` */
+
+DROP TABLE IF EXISTS `ks_question`;
+
+CREATE TABLE `ks_question` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `qid` varchar(200) NOT NULL COMMENT '问题id',
+  `title` varchar(200) NOT NULL COMMENT '问题标题',
+  `content` longtext NOT NULL COMMENT '问题内容',
+  `status` int(1) NOT NULL DEFAULT '0' COMMENT '状态 0 未解决 1 已解决',
+  `sort` int(1) NOT NULL DEFAULT '0' COMMENT '排序 0 普通  1 置顶',
+  `views` int(10) NOT NULL DEFAULT '0' COMMENT '浏览量',
+  `author_id` varchar(200) NOT NULL COMMENT '作者id',
+  `author_name` varchar(200) NOT NULL COMMENT '作者名',
+  `author_avatar` varchar(500) NOT NULL COMMENT '作者头像',
+  `category_id` int(10) NOT NULL COMMENT '问题分类id',
+  `category_name` varchar(50) NOT NULL COMMENT '问题分类名称',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_update` datetime NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_question_category` */
+
+DROP TABLE IF EXISTS `ks_question_category`;
+
+CREATE TABLE `ks_question_category` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `category` varchar(50) NOT NULL COMMENT '问题分类',
+  `gmt_create` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_say` */
+
+DROP TABLE IF EXISTS `ks_say`;
+
+CREATE TABLE `ks_say` (
+  `id` varchar(200) NOT NULL COMMENT '唯一id',
+  `title` varchar(200) NOT NULL COMMENT '标题',
+  `content` varchar(5000) NOT NULL COMMENT '内容',
+  `gmt_create` datetime NOT NULL COMMENT '时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_user` */
+
+DROP TABLE IF EXISTS `ks_user`;
+
+CREATE TABLE `ks_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `uid` varchar(200) NOT NULL COMMENT '用户编号',
+  `role_id` int(10) NOT NULL COMMENT '角色编号',
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `password` varchar(200) NOT NULL COMMENT '密码',
+  `avatar` varchar(500) NOT NULL DEFAULT '/images/avatar/avatar-1.jpg' COMMENT '头像',
+  `login_date` datetime NOT NULL COMMENT '登录时间',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `deleted` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_user_donate` */
+
+DROP TABLE IF EXISTS `ks_user_donate`;
+
+CREATE TABLE `ks_user_donate` (
+  `uid` varchar(200) NOT NULL COMMENT '用户id',
+  `donate_json` varchar(2000) NOT NULL COMMENT '赞赏二维码信息'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_user_info` */
+
+DROP TABLE IF EXISTS `ks_user_info`;
+
+CREATE TABLE `ks_user_info` (
+  `uid` varchar(200) NOT NULL COMMENT '用户id',
+  `nickname` varchar(80) DEFAULT NULL COMMENT '用户昵称',
+  `realname` varchar(80) DEFAULT NULL COMMENT '真实姓名',
+  `qq` varchar(20) DEFAULT NULL COMMENT 'QQ',
+  `wechat` varchar(200) DEFAULT NULL COMMENT 'WeChat',
+  `email` varchar(500) DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(20) DEFAULT NULL COMMENT '手机',
+  `work` varchar(200) DEFAULT NULL COMMENT '工作',
+  `address` varchar(500) DEFAULT NULL COMMENT '地址',
+  `hobby` varchar(500) DEFAULT NULL COMMENT '爱好',
+  `intro` varchar(2000) DEFAULT NULL COMMENT '自我介绍',
+  `deleted` int(1) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ks_user_role` */
+
+DROP TABLE IF EXISTS `ks_user_role`;
+
+CREATE TABLE `ks_user_role` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '角色编号',
+  `name` varchar(200) NOT NULL COMMENT '角色名称',
+  `description` varchar(500) NOT NULL DEFAULT '无描述...' COMMENT '角色描述',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_blog` */
+
+DROP TABLE IF EXISTS `t_blog`;
+
+CREATE TABLE `t_blog` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `appreciation` bit(1) DEFAULT NULL,
+  `commentabled` bit(1) DEFAULT NULL,
+  `content` longtext COLLATE utf8mb4_bin,
+  `create_time` datetime DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `first_picture` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `flag` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `published` bit(1) DEFAULT NULL,
+  `recommend` bit(1) DEFAULT NULL,
+  `share_statement` bit(1) DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `views` bigint(20) DEFAULT NULL,
+  `type_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK292449gwg5yf7ocdlmswv9w4j` (`type_id`),
+  KEY `FK8ky5rrsxh01nkhctmo7d48p82` (`user_id`),
+  CONSTRAINT `FK292449gwg5yf7ocdlmswv9w4j` FOREIGN KEY (`type_id`) REFERENCES `t_type` (`id`),
+  CONSTRAINT `FK8ky5rrsxh01nkhctmo7d48p82` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Table structure for table `t_blog_tags` */
+
+DROP TABLE IF EXISTS `t_blog_tags`;
+
+CREATE TABLE `t_blog_tags` (
+  `blogs_id` bigint(20) NOT NULL,
+  `tags_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`blogs_id`,`tags_id`),
+  KEY `FK5feau0gb4lq47fdb03uboswm8` (`tags_id`),
+  CONSTRAINT `FK5feau0gb4lq47fdb03uboswm8` FOREIGN KEY (`tags_id`) REFERENCES `t_tag` (`id`),
+  CONSTRAINT `FKh4pacwjwofrugxa9hpwaxg6mr` FOREIGN KEY (`blogs_id`) REFERENCES `t_blog` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Table structure for table `t_comment` */
+
+DROP TABLE IF EXISTS `t_comment`;
+
+CREATE TABLE `t_comment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `admin_comment` bit(1) DEFAULT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `content` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `nickname` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `blog_id` bigint(20) DEFAULT NULL,
+  `parent_comment_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKke3uogd04j4jx316m1p51e05u` (`blog_id`),
+  KEY `FK4jj284r3pb7japogvo6h72q95` (`parent_comment_id`),
+  CONSTRAINT `FK4jj284r3pb7japogvo6h72q95` FOREIGN KEY (`parent_comment_id`) REFERENCES `t_comment` (`id`),
+  CONSTRAINT `FKke3uogd04j4jx316m1p51e05u` FOREIGN KEY (`blog_id`) REFERENCES `t_blog` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Table structure for table `t_tag` */
+
+DROP TABLE IF EXISTS `t_tag`;
+
+CREATE TABLE `t_tag` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Table structure for table `t_type` */
+
+DROP TABLE IF EXISTS `t_type`;
+
+CREATE TABLE `t_type` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `avatar` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `nickname` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Table structure for table `yy_friend` */
+
+DROP TABLE IF EXISTS `yy_friend`;
+
+CREATE TABLE `yy_friend` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) DEFAULT NULL,
+  `img` varchar(200) DEFAULT NULL,
+  `birthday` datetime DEFAULT NULL,
+  `sex` varchar(1) DEFAULT NULL,
+  `myself` varchar(200) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `gmt_create` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
